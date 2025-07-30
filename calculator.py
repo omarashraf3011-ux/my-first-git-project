a75da4c036abf89ea56afcff65fe156e3676c1b5
Omar <omar.ashraf3011@gmail.com>
2025-07-30 06:16:37 -0400

    adding multiplying function

--- calculator.py
+++ calculator.py
@@ -4,5 +4,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 print(f"Adding 5 and 3: {add(5, 3)}")
 print(f"Subtracting 10 and 4: {subtract(10, 4)}")
+print(f"Multiplying 6 and 7: {multiply(6, 7)}")
+
